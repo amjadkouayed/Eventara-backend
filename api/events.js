30a15f4443dcd155dcf1e47a998e5e6738e92d75
Amjad Kouayed <amjadkouayed@gmail.com>
2025-01-04 17:52:29 +0100

    add update event route and get single event

--- api/events.js
+++ api/events.js
@@ -1,32 +1,31 @@
 const express = require("express")
 const router = express.Router()
 const pool = require("../db")
-const {getEvents, createEvent, deleteEvent} = require("../service/events-service")
+const {getEvents, createEvent, deleteEvent, getSingleEvent, updateEvent} = require("../service/events-service")
 const passport = require("passport")
 
 
 router.use(passport.authenticate("jwt", {session: false}))
 
 
-// http://localhost:4000/events/create
-router.post("/create", async (req,res) => {
-    const { title, description, date, location} = req.body
+
+router.post("/", async (req,res) => {
+    const {title, description, date, location} = req.body
+    const user_id = req.user.id
 
     try {
-
-        const user_id = req.user.id
         const result = await createEvent(title, description, date, location, user_id)
 
-        res.status(201).json({ message: "event successfully created", event_id: result.rows[0].id })
+        res.status(201).json({ message: "event successfully created", event_id: result.id })
 
     } catch(err) {
-        console.log(err.message)
+        console.error("eror creating event", err.message)
         res.status(500).json({ error: "Internal server error" })
     }
 
 })
-// http://localhost:4000/events/list
-router.get("/list", async (req,res) => {   
+
+router.get("/", async (req,res) => {   
 
 
     try {
@@ -36,17 +35,17 @@ router.get("/list", async (req,res) => {
         }
 
         const result = await getEvents(user_id)
-        res.json(result.rows)
+        res.json(result)
 
     }catch(err) {
-        console.log(err.message)
+        console.error("error getting events")
         res.status(500).json({ error: "Internal server error" })
     }
 })
 
 
-// http://localhost:4000/events/delete/id
-router.delete("/delete/:event_id", async(req,res) => {
+
+router.delete("/:event_id", async(req,res) => {
     
     try{
     const event_id = req.params.event_id
@@ -55,26 +54,58 @@ router.delete("/delete/:event_id", async(req,res) => {
     const result = await deleteEvent(event_id, user_id)
 
     if (result.error) {
-        return res.status(404).json({ error: result.error })
+        return res.status(404).json(result.error)
     }
 
     res.json(result)
     
     }catch(err) {
-        console.error("Error deleting event:", err.message)
+        console.error("Error deleting event")
         res.status(500).json({ error: "Internal server error" })
     }
  
 })
 
 //create a route to return one event
-router.get("/list/:event_id", async(req, res) => {
+router.get("/:event_id", async(req, res) => {
+    const event_id = req.params.event_id
+    const user_id = req.user.id
 
+    try {
+        const result = await getSingleEvent(event_id, user_id)
+        
+        if (result.error){
+            return res.status(400).json(result.error)
+        }
 
+        res.status(200).json(result)
 
+    } catch (err) {
+        console.error("error getting event")
+        res.status(500).json({ error: "Internal server error"})
+    }
 } )
 
 //create update route
+router.put("/:event_id", async (req, res) => {
+    const event_id = req.params.event_id
+    const user_id = req.user.id
+    const updateData = req.body
+
+    try{
+        const result = await updateEvent(event_id, user_id, updateData)
+        console.log(result)
+
+        if (result.error){
+            return res.status(400).json(result.error)
+        }
+        res.status(200).json({message: "event updated successfuly", new_values : result})
+
+    }catch(err) {
+        console.error("error getting event")
+        res.status(500).json({ error: "Internal server error"})
+    }
+})
 
 
 module.exports = router
--- service/events-service.js
+++ service/events-service.js
@@ -4,7 +4,7 @@ module.exports.getEvents = async (user_id) => {
 
     const result = await pool.query('SELECT * FROM events WHERE user_id = $1', [user_id])
 
-    return result
+    return result.rows
 
 }
 
@@ -14,7 +14,8 @@ module.exports.createEvent = async (title, description, date, location, user_id)
     const createQuery = `INSERT INTO events (title, description, date, location, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING id`
     const result = await pool.query(createQuery, [title, description, date, location, user_id])
 
-    return result
+    console.log(result.rows)
+    return result.rows
 }
 
 
@@ -36,4 +37,61 @@ module.exports.deleteEvent = async (event_id, user_id) => {
     await pool.query(deleteQuery, [event_id, user_id]);
 
     return { message: "Event deleted successfully" };  
+}
+
+module.exports.getSingleEvent = async (event_id, user_id) => {
+
+    if(!event_id || !user_id) {
+        return ({error:"Event Id and User Id required"})
+    }
+
+    const getEventResult = await pool.query("SELECT * FROM events WHERE ID = $1 AND user_id = $2", [event_id, user_id])
+
+    if (getEventResult.rows.length === 0){
+        return {error: "event not found or you are not the owner"}
+    }
+
+    return getEventResult.rows[0]
+}
+
+module.exports.updateEvent = async (event_id, user_id, updateData) => {
+    if (!event_id || !user_id) {
+        return { error: "Event Id and User Id are required" };
+    }
+
+    const { title, description, date, location} = updateData
+
+    const fields = []
+    const values = []
+    let counter = 1
+
+    //update query logic
+    if (title){
+        fields.push(`title = $${counter++}`) // title = $1 (uses the value of the counter first, then increments)
+        values.push(title)
+    }
+    if (description) {
+        fields.push(`description = $${counter++}`)
+        values.push(description)
+    }
+    if (date) {
+        fields.push(`date = $${counter++}`)
+        values.push(date)
+    }
+    if (location) {
+        fields.push(`location = $${counter++}`)
+        values.push(location)
+    }
+
+    values.push(event_id, user_id)
+
+    const query = ` UPDATE events SET ${fields.join(", ")} WHERE id = $${counter++} AND user_id = $${counter} RETURNING *; `
+
+    const result = await pool.query(query, values)
+
+    if (result.rows.length === 0){
+        return {error: "event not found or you are not the owner"}
+    }
+
+    return result.rows[0]
 }
