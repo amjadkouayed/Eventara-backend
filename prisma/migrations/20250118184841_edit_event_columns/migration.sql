0496f87a9e17a2bd6f93506524249eea0f304f61
Amjad Kouayed <amjadkouayed@gmail.com>
2025-01-23 01:39:08 +0100

    Stop tracking the prisma folder

--- prisma/migrations/20250118184841_edit_event_columns/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `created_at` on the `events` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "events" DROP COLUMN "created_at";
