0140b81bce22a14c0181a0d7cb6d131be30955e2
Amjad Kouayed <amjadkouayed@gmail.com>
2025-02-11 07:27:47 +0100

    fix authentication bug

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,48 +1,41 @@
-const fs = require('fs');
-const path = require('path');
-const JwtStrategy = require("passport-jwt").Strategy
-const ExtractJwt = require("passport-jwt").ExtractJwt
-const {PrismaClient} = require('@prisma/client')
+const fs = require("fs");
+const path = require("path");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const { PrismaClient } = require("@prisma/client");
 
 const pathToKey = path.join(__dirname, "public-key.pem");
-const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
+const PUB_KEY = fs.readFileSync(pathToKey, "utf8");
 
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const options = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: PUB_KEY,
-    algorithms: ["RS256"]
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ["RS256"],
 };
 
 const strategy = new JwtStrategy(options, async (payload, done) => {
-    const user_id = payload.sub
-    try {
-        const result = prisma.users.findUnique({
-            where: {
-                id: user_id
-            }
-        })
-        console.log(result)
-        if (result.length === 0) {
-            return done(null, false);
-        }
-
-        const user = result[0]
-
-        return done(null, user)
-
-
-    } catch(error) {
-        return done(error, null)
-
+  const user_id = payload.sub;
+  try {
+    const result = await prisma.users.findUnique({
+      where: {
+        id: user_id,
+      },
+    });
+    console.log(result);
+    if (result.length === 0) {
+      return done(null, false);
     }
-    
 
-})
+    const user = result;
+
+    return done(null, user);
+  } catch (error) {
+    return done(error, null);
+  }
+});
 
 module.exports.configureJwtPassport = (passport) => {
-    passport.use(strategy)
-}
-
+  passport.use(strategy);
+};
